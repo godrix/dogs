c9f969348d749b7d929d3a11ec368f6b20e54d38
Carlos Godri <cggodri@gmail.com>
2020-07-31 14:34:24 -0300

    getUser

--- src/pages/SigIn/index.tsx
+++ src/pages/SigIn/index.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent} from 'react';
+import React, { FormEvent, useEffect} from 'react';
 import api from '../../services/api';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -8,24 +8,51 @@ import {useForm} from '../../hooks/useForm';
 
 const SigIn: React.FC = () => {
   const username = useForm({type:false});
-  const password = useForm({type:'email'});
+  const password = useForm({type:false});
 
-  function handleSubmit(event:FormEvent){
-    api.post('/jwt-auth/v1/token',{
-      username,
-      password
-    }).then(response =>{
-      console.log(response)
-    })
-    event.preventDefault();
+  async function getUser(){
+    await api.get('/api/user')
   }
+
+  async function handleSubmit(event:FormEvent){
+    event.preventDefault();
+
+    if(username.validate() && password.validate()){
+      try {
+        const response = await api.post('/jwt-auth/v1/token',{
+          username: username.value,
+          password: password.value
+        });
+
+        window.localStorage.setItem('@dogs:token', response.data.json);
+        api.defaults.headers.common['Authorization'] = `Bearer ${response.data.json}`;
+        getUser();
+      } catch (error) {
+        
+      }
+
+      
+    }
+
+  }
+
+  useEffect(()=>{
+    const token = window.localStorage.getItem('@dogs:token');
+
+    if(token){
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+      getUser();
+
+    }
+
+  },[])
 
   return (
     <section>
       <h1>Login</h1>
       <form onSubmit={handleSubmit}>
-        <Input label="Usuario" name="user" type="email" {...username}/>
-        <Input label="Senha" name="password" type="email" {...password}/>
+        <Input label="Usuario" name="user" type="text" {...username}/>
+        <Input label="Senha" name="password" type="text" {...password}/>
         <Button>Entrar</Button>
       </form>
     </section>
